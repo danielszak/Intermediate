ac8804f4f2b95d9663c5b112a34892bad7f7edd3
danielszak <61319201+danielszak@users.noreply.github.com>
2020-03-31 12:02:17 +0200

    Add files via upload

---
+++ Stack/Stack/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack stack = new Stack();
+            stack.Start();
+        }
+    }
+}
---
+++ Stack/Stack/Stack.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Stack
+{
+    public class Stack
+    {
+        private List<object> objList = new List<object>();
+        public Stack()
+        {
+
+        }
+
+        public void Start()
+        {
+            Stack stack = new Stack();
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+
+
+        }
+        private void Push(Object obj)
+        {
+            if (obj == null)
+                throw new InvalidOperationException("There is nothing in the stack");
+
+            objList.Add(obj);
+
+        }
+
+        private object Pop()
+        {
+            if(objList.Count == 0)
+            {
+                throw new InvalidOperationException("There is nothing in the stack");
+            }
+
+           
+            int index = objList.Count-1;
+            Object tBack = objList[index];
+            objList.RemoveAt(index);
+            return (tBack);
+        }
+
+        private void Clear()
+        {
+            objList.Clear();
+        }
+
+    }
+}
